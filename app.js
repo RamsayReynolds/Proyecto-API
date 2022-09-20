3914898417acaec82b93cf097c46e020642f1075
RamsayReynolds <renorenos@hotmail.com>
2022-09-19 20:45:52 -0500

    Mapas V1.1

--- app.js
+++ app.js
@@ -1,21 +1,87 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoibWp2YWxlbnp1ZWxhIiwiYSI6ImNrb2Fmdm9zZDBpM28ybnFtYTQ2Z2MwMnYifQ.ZY3jTw0-6tjUSOOJXJHsdw'
+mapboxgl.accessToken = 'pk.eyJ1IjoicmFtc2F5cmV5bm9sZHMiLCJhIjoiY2w4OWdra29yMDZ5MjN3bXhqczBtZjZ4eiJ9.hk3Lu51KKcfj_kPSZ8bMfg'
 
-var antesMap = new mapboxgl.Map({
-	container: 'antes',
-	style: 'mapbox://styles/mapbox/light-v10',
-	center: [-74.082412,4.639386],
-	zoom: 9
+var map = new mapboxgl.Map({
+	container: 'map',
+	style: 'mapbox://styles/mapbox/streets-v11',
+	center: [-98.198160,19.043700],
+	zoom: 11.5
 });
 
-var despuesMap = new mapboxgl.Map({
-	container: 'despues',
-	style: 'mapbox://styles/mapbox/dark-v10',
-	center: [-74.082412,4.639386],
-	zoom: 9
+document
+.getElementById('listing-group')
+.addEventListener('change', function(e)
+{
+var handler = e.target.id;
+if(e.target.checked){
+	map[handler].enable();
+} else {
+	map[handler].disable();
+}
 });
 
-var contanier = '#compare-container';
-
-var map = new mapboxgl.Compare(antesMap,despuesMap,contanier,{
-	//mousemove: true
-})
+var customData = {
+	'features': [
+	{
+		'type': 'Feature',
+		'properties': {
+			'title': 'Tec Campus Puebla'
+		},
+	'geometry': {
+		'coordinates': [-98.241363,19.018283],
+		'type': 'Point'
+		}
+	},
+	{
+		'type': 'Feature',
+		'properties': {
+			'title': 'Tec Campus Santa Fe'
+		},
+	'geometry': {
+		'coordinates': [-99.258462,19.359369],
+		'type': 'Point'
+		}
+	},
+	{
+		'type': 'Feature',
+		'properties': {
+			'title': 'Tec Campus Estado de México'
+		},
+	'geometry': {
+		'coordinates': [-99.227920,19.594956],
+		'type': 'Point'
+		}
+	}
+	],
+	'type': 'FeatureCollection'
+};
+ 
+function forwardGeocoder(query) {
+	var matchingFeatures = [];
+	for (var i = 0; i < customData.features.length; i++) {
+		var feature = customData.features[i];
+		// Handle queries with different capitalization
+		// than the source data by calling toLowerCase().
+		if (
+			feature.properties.title
+				.toLowerCase()
+				.search(query.toLowerCase()) !== -1
+		) {
+			feature['place_name'] = '🐏 ' + feature.properties.title;
+			feature['center'] = feature.geometry.coordinates;
+			feature['place_type'] = ['landmark'];
+			matchingFeatures.push(feature);
+			}
+	}
+	return matchingFeatures;
+}
+ 
+//Add the control to the map.
+map.addControl(
+	new MapboxGeocoder({
+		accessToken: mapboxgl.accessToken,
+		localGeocoder: forwardGeocoder,
+		zoom: 16,
+		placeholder: '¿Qué lugar buscas?',
+		mapboxgl: mapboxgl
+	})
+);
